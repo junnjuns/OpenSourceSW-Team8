650576962ef759d9eda6759939412788dd4de9bc
junnjuns <donamjun1@naver.com>
2022-12-13 03:02:45 +0900

    feat(image):add function copying all image to one folder

---
+++ copy_image.py
@@ -0,0 +1,11 @@
+from glob import glob
+from shutil import copyfile
+
+import tqdm as tqdm
+
+def copy():
+    output = glob('datas/**', recursive=True)
+
+    for i in tqdm.tqdm(output):
+        if i.endswith(".jpg"):
+            copyfile(i, f'/images/{i.split("/")[-1]}')
