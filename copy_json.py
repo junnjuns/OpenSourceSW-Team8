00f43149f08019f6c21471efa4a4f4a7dc9529cf
junnjuns <donamjun1@naver.com>
2022-12-13 03:07:52 +0900

    feat(annotation):add function copying all annotation to one folder

---
+++ copy_json.py
@@ -0,0 +1,12 @@
+from glob import glob
+from shutil import copyfile
+
+import tqdm as tqdm
+
+def copy():
+    output = glob('datas/**', recursive=True)
+    count = 0
+    for i in tqdm.tqdm(range(len(output))):
+        if output[i].endswith(".json"):
+            copyfile(output[i], f'/ano/{count}.json')
+            count += 1
